0c93ffbe306e0e7beb9bfd065180a0b427f4ebda
Uditha DeSilva <uditha.i.desilva@gmail.com>
2021-06-06 13:48:19 +0100

    Updated to fetch the PDF file from Royal Mail website

--- bulkCoP.py
+++ bulkCoP.py
@@ -3,6 +3,7 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from os import path
+import urllib.request
 
 x = [40, 190, 420]  # Offsets of "Name","Address","Class" columns
 y0 = 705            # offset of 1st line
@@ -10,6 +11,7 @@ fontName,fontSize = "Helvetica",9.5
 yoffs = -15.5       # gap between lines
 pageCount = 0
 countX, countY = 350, 150
+origPDFurl = "https://www.royalmail.com/sites/default/files/Bulk_Certificate_Posting_Standard%20MAR14.pdf"
 origPDFfname = "./Bulk_Certificate_Posting_Standard%20MAR14.pdf"
 newPageExt = "_BulkCoP.pdf"
 
@@ -66,12 +68,24 @@ outfile = basename + newPageExt
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
 
-try:
-    pdfpath = path.abspath(path.join(path.dirname(__file__), origPDFfname))
-    existing_pdf = PdfFileReader(open(pdfpath, "rb"))
-    print("Reading template from {}".format(origPDFfname))
-except TypeError:
-    print("unexpected")
+pdfpath = path.abspath(path.join(path.dirname(__file__), origPDFfname))
+
+while True:
+    try:
+        existing_pdf = PdfFileReader(open(pdfpath, "rb"))
+        print("Reading template from {}".format(origPDFfname))
+        break
+    except FileNotFoundError:
+        print("Trying to fetch {}".format(origPDFurl))
+        urllib.request.urlretrieve(origPDFurl,origPDFfname)
+        print("Success. Retrying file read...")
+        # try:
+        #     urllib.retrieve(origPDFurl,origPDFfname)
+        #     continue
+        # except:
+        #     print("Could not fetch {}".format(origPDFurl))
+        #     exit(1)
+
 
 origpage = existing_pdf.getPage(0)
 
