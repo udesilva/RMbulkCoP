ba1512c210c12ae66844d2400572fbb10f57d074
Uditha DeSilva <uditha.i.desilva@gmail.com>
2021-06-06 13:08:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+bulkCoP
+=======
+
+A script to generate Royal Mail "Bulk Certificate of Posting" pages from a CSV file containing name, address, and post class ("First" or "Second").
+
+Requires a copy of the Royal Mail PDF to be in the same directory as the script:
+
+https://www.royalmail.com/sites/default/files/Bulk_Certificate_Posting_Standard%20MAR14.pdf
+
+TODO: Download the template PDF if no local copy found.
+
+Licensed per GNU GPL.
+
+Uditha Desilva uditha.i.desilva@gmail.com
+06/06/2021
---
+++ bulkCoP.py
@@ -0,0 +1,88 @@
+import io,sys,csv,copy,os
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from os import path
+
+x = [40, 190, 420]  # Offsets of "Name","Address","Class" columns
+y0 = 705            # offset of 1st line
+fontName,fontSize = "Helvetica",9.5
+yoffs = -15.5       # gap between lines
+pageCount = 0
+countX, countY = 350, 150
+origPDFfname = "./Bulk_Certificate_Posting_Standard%20MAR14.pdf"
+newPageExt = "_BulkCoP.pdf"
+
+def newPage():
+    global can,y,count,fontName,fontSize
+    can.setFont(fontName,fontSize)
+    y = y0
+    count = 0
+
+def flushPage():
+    global can,count,pageCount, countX, countY
+    if count > 0:
+        pageCount += 1
+        print("Writing page {}".format(pageCount))
+        can.drawString(countX, countY, str(count))  # Add the "Number of Items"
+        can.showPage()
+        newPage()
+
+packet = io.BytesIO()
+
+# create a new PDF with Reportlab
+can = canvas.Canvas(packet, pagesize=A4)
+
+try:
+    csvFname = sys.argv[1]
+except IndexError:
+    print("Must provide a CSV filename")
+    exit(1)
+
+with open(csvFname, newline='') as csvfile:
+    print("Reading from {}".format(csvFname))
+    reader = csv.reader(csvfile)
+    newPage()
+    for row in reader:
+        if row[0] == "Name": # skip an actual "name" label
+            continue
+
+        for j in range(0, 3):
+            can.drawString(x[j], y, row[j])
+        y += yoffs
+
+        count += 1
+        if(count == 30):
+           flushPage()
+
+flushPage() # push any remaining stuff in canvas out
+can.save()  # close the canvas
+
+output = PdfFileWriter()
+basename,ext = os.path.splitext(csvFname)
+outfile = basename + newPageExt
+
+# move to the beginning of the StringIO buffer
+packet.seek(0)
+new_pdf = PdfFileReader(packet)
+
+try:
+    pdfpath = path.abspath(path.join(path.dirname(__file__), origPDFfname))
+    existing_pdf = PdfFileReader(open(pdfpath, "rb"))
+    print("Reading template from {}".format(origPDFfname))
+except TypeError:
+    print("unexpected")
+
+origpage = existing_pdf.getPage(0)
+
+for i in range(0,pageCount):
+    page = copy.copy(origpage) # make a copy of the origpage to avoid trashing it
+    page.mergePage(new_pdf.getPage(i))
+    output.addPage(page)
+
+print("Writing " + outfile)
+outputStream = open(outfile, "wb")
+output.write(outputStream)
+outputStream.close()
+
+
